b580af1bad2b69d89f269dca56128eca6aa26c70
Coto Cisternas <cotocisternas@gmail.com>
2017-02-08 12:34:38 -0300

    new repo

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,26 +1,25 @@
 # Setup the official repo
 class beats::repo::apt() {
+
+  include ::apt
+  Class['apt::update'] -> Package['metricbeat']
+  Class['apt::update'] -> Package['filebeat']
+  Class['apt::update'] -> Package['packetbeat']
+
   apt::source { 'elastic-beats':
     comment  => 'Official Elastic beats repositories',
-    location => 'https://packages.elastic.co/beats/apt',
+    location => 'https://artifacts.elastic.co/packages/5.x/apt',
     release  => 'stable',
     repos    => 'main',
     key      => {
       id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-      server => 'pgp.mit.edu'
+      source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
     },
     include  => {
-      'deb'  => true
+      'deb'  => true,
+      'src'  => false,
     }
   }
-  exec {'apt-get update':
-    command => 'apt-get -qq update',
-    path => '/usr/bin',
-    unless => ['/usr/bin/dpkg -l | /bin/grep apt-transport-https']
-  }
-  package {'apt-transport-https':
-    ensure  => latest,
-    before  => Apt::Source['elastic-beats'],
-    require => Exec['apt-get update']
-  }
+
+
 }
