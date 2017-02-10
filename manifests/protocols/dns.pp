e2dab8186ccd037c218c3ced05a97dfee3467bd4
Coto Cisternas <cotocisternas@gmail.com>
2017-02-10 15:06:47 -0300

    true for dns

--- manifests/protocols/dns.pp
+++ manifests/protocols/dns.pp
@@ -1,6 +1,8 @@
 # DNS
 class beats::protocols::dns (
-  $ports    = ['53']
+  $ports    = ['53'],
+  $include_authorities = true,
+  $include_additionals = true
 ) {
   concat::fragment {'protocols-dns':
     target  => '/etc/packetbeat/packetbeat.yml',
